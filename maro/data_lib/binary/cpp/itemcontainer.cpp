874ad194348b64cc79b60ae0ea79f3848e72abba
chaosyu <chaos.you@gmail.com>
2020-11-25 10:28:45 +0800

    use macro for dup code

--- maro/data_lib/binary/cpp/itemcontainer.cpp
+++ maro/data_lib/binary/cpp/itemcontainer.cpp
@@ -5,7 +5,8 @@ namespace maro
     namespace datalib
     {
         ItemContainer::ItemContainer()
-        {}
+        {
+        }
 
         void ItemContainer::set_buffer(char *buffer)
         {
@@ -22,65 +23,21 @@ namespace maro
             _offset = offset;
         }
 
-        template <>
-        int32_t ItemContainer::get<int32_t>(int offset)
-        {
-            int32_t r = 0;
+#define Getter(type)                                          \
+    template <>                                               \
+    type ItemContainer::get<type>(int offset)                 \
+    {                                                         \
+        type r = 0;                                           \
+        memcpy(&r, &_buffer[_offset + offset], sizeof(type)); \
+        return r;                                             \
+    }
 
-            memcpy(&r, &_buffer[ _offset + offset], sizeof(int32_t));
-
-            return r;
-        }
-
-        template <>
-        float ItemContainer::get<float>(int offset)
-        {
-            float r = 0;
-
-            memcpy(&r, &_buffer[ _offset + offset], sizeof(float));
-
-            return r;
-        }
-
-        template <>
-        double ItemContainer::get<double>(int offset)
-        {
-            double r = 0;
-
-            memcpy(&r, &_buffer[ _offset + offset], sizeof(double));
-
-            return r;
-        }
-
-        template <>
-        short ItemContainer::get<short>(int offset)
-        {
-            short r = 0;
-
-            memcpy(&r, &_buffer[ _offset + offset], sizeof(short));
-
-            return r;
-        }
-
-        template <>
-        LONGLONG ItemContainer::get<LONGLONG>(int offset)
-        {
-            LONGLONG r = 0LL;
-
-            memcpy(&r, &_buffer[ _offset + offset], sizeof(LONGLONG));
-
-            return r;
-        }
-
-        template <>
-        ULONGLONG ItemContainer::get<ULONGLONG>(int offset)
-        {
-            ULONGLONG r = 0ULL;
-
-            memcpy(&r, &_buffer[ _offset + offset], sizeof(ULONGLONG));
-
-            return r;
-        }
+        Getter(int32_t)
+        Getter(float)
+        Getter(double)
+        Getter(short)
+        Getter(LONGLONG)
+        Getter(ULONGLONG)
     } // namespace datalib
 
 } // namespace maro
